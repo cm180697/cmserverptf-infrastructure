3cc19d43a6a72d26fc90b73ee86c43aa4edf0c43
cm180697 <camilomateuso@hotmail.com>
2025-07-29 22:51:57 -0500

    feat(monitoring): add cloudwatch dashboard for backend

--- modules/lambda-api/outputs.tf
+++ modules/lambda-api/outputs.tf
@@ -2,3 +2,13 @@ output "api_endpoint_url" {
   description = "The invoke URL for the visitor counter API."
   value       = aws_apigatewayv2_api.lambda_api.api_endpoint
 }
+
+output "api_id" {
+  description = "The ID of the API Gateway."
+  value       = aws_apigatewayv2_api.lambda_api.id
+}
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function."
+  value       = aws_lambda_function.visitor_counter_lambda.function_name
+}
